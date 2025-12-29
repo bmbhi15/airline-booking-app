d5634d7194e83eeebd5b2d841520418d28fb265c
bmbhi15 <admin@MacBook-Air.local>
2025-12-29 18:23:16 +0530

    Started the express server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require("express");
+const { PORT } = require("./src/config");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Started my first server");
+});
+app.listen(PORT);
